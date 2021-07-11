8587e010d18ec3defb3f93a27bff3296358a9402
MuAladdinIbrahim <muhammadalaa27@gmail.com>
2021-07-11 22:42:18 +0200

    rename dir RawNotification

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import router from "./modules/Notification/http/router"
+import router from "./modules/RawNotification/http/router"
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
